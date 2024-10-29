7c1243b7ffd6adca2a569ead42e9d7ff1b411516
Alban Billiette <albillie@c1r4p6.42lehavre.fr>
2024-10-29 09:53:22 +0100

    UPDATE

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,36 @@
 /*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 01:26:03 by albillie          #+#    #+#             */
-/*   Updated: 2024/10/28 03:26:44 by albillie         ###   ########.fr       */
+/*   Updated: 2024/10/29 09:42:00 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	ft_read_line()
+{
+	
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
-	size_t		i;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	read(fd, buffer, BUFFER_SIZE);
+	printf("%s", buffer);
+	return (buffer);
+}
 
+int main()
+{
+	int fd = open("test.txt", O_RDONLY);
+	get_next_line(fd);
+	get_next_line(fd);
+	get_next_line(fd);
+	get_next_line(fd);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 01:26:00 by albillie          #+#    #+#             */
-/*   Updated: 2024/10/28 06:49:29 by albillie         ###   ########.fr       */
+/*   Updated: 2024/10/29 07:48:06 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,34 +26,46 @@ int	ft_strlen(char *str)
 char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*result;
-	size_t	s1_len;
-	size_t	s2_len;
 	size_t	i;
+	size_t	j;
 
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	result = malloc(s1_len + s2_len + 1);
+	result = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!result)
-	{
 		return (NULL);
-	}
 	i = 0;
 	while (s1[i])
 	{
 		result[i] = s1[i];
 		i++;
 	}
-	printf("%ld\n", i);
+	j = 0;
+	while (s2[j])
+	{
+		result[i + j] = s2[j];
+		j++;
+	}
+	result[i + j] = '\0';
 	return (result);
 }
 
+char	*ft_strchr(const char *s, char c)
+{
+	size_t	i;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			return (char *) s[i];
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+
+
 int main()
 {
-	char *string;
-	char *string1;
-
-	string = "j'adore les chats";
-	string1 = "j'adore les chats";
-
-	printf("%s\n", ft_strjoin(string, string1));
+	printf("%s\n", ft_strchr("j'adore", 'a'));
 }

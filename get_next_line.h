c10a087b0ac9f522a72e2de0b83caa8e5321af83
albillie <albillie@DESKTOP-T76U5HK.>
2024-10-30 08:41:09 +0100

    UPDATWE

--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,27 @@
 /*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 01:26:05 by albillie          #+#    #+#             */
-/*   Updated: 2024/10/29 09:03:39 by albillie         ###   ########.fr       */
+/*   Updated: 2024/10/30 06:54:15 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE
 #define GET_NEXT_LINE
 
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 42
+#endif
+
 # include <stdio.h>
 # include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-# define BUFFER_SIZE 1
-
 char	*get_next_line(int fd);
-
-
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(const char *str);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,13 +6,13 @@
 /*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 01:26:00 by albillie          #+#    #+#             */
-/*   Updated: 2024/10/29 07:48:06 by albillie         ###   ########.fr       */
+/*   Updated: 2024/10/30 08:40:48 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_strlen(char *str)
+int	ft_strlen(const char *str)
 {
 	size_t	i;
 	i = 0;
@@ -48,24 +48,72 @@ char	*ft_strjoin(char *s1, char *s2)
 	return (result);
 }
 
-char	*ft_strchr(const char *s, char c)
+char	*ft_strchr(const char *s, int c)
 {
-	size_t	i;
+	char	cc;
 
-	while (s[i])
+	cc = (char) c;
+	if (!cc)
 	{
-		if (s[i] == c)
+		return ((char *) s + ft_strlen(s));
+	}
+	while (*s)
+	{
+		if (*s == cc)
 		{
-			return (char *) s[i];
+			return ((char *) s);
 		}
-		i++;
+		s++;
 	}
 	return (NULL);
 }
 
-
-
-int main()
+char	*ft_strdup(const char *s)
 {
-	printf("%s\n", ft_strchr("j'adore", 'a'));
+	char	*result;
+	size_t	i;
+	size_t	s_len;
+
+	s_len = ft_strlen(s) + 1;
+	result = malloc(s_len);
+	if (!result)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (s[i])
+	{
+		result[i] = s[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*copy;
+	size_t	i;
+	size_t	s_len;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+	{
+		len = s_len - start;
+	}
+	copy = (char *) malloc ((len + 1));
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		copy[i] = s[start + i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
 }
